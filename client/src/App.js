de3977f05c560ff28b966a02733edcc6af64be04
Monika Grzanek <monski@o2.pl>
2022-03-13 15:22:37 +0100

    Add listeners to the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+//const socket = io();
+
 
 class App extends React.Component {
-
+  
   render() {
     return (
       <div className="App">
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const socket = require('socket.io');
 
 const app = express();
 
+const tasks = [];
+
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running...');
 });
@@ -10,4 +12,23 @@ const server = app.listen(process.env.PORT || 8000, () => {
 
 app.use((req, res) => {
     res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    io.to(socket.id).emit("updateData", tasks);
+    console.log('New user:', socket.id );
+
+    socket.on('addTask', (newTask) => {
+        tasks.push(newTask);
+        socket.broadcast.emit('addTask', tasks);
+        console.log('Actually tasks:', tasks);
+    });
+
+    socket.on('removeTask', (indexOfRemoveElement) => {
+        tasks.splice(indexOfRemoveElement, 1);
+        socket.broadcast.emit('removeTask', tasks);
+        console.log('Actually tasks:', tasks);
+    })
 });
